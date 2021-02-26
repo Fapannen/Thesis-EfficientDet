aa304930b0fa3a6c236bf9f30499087ee245e45a
Tomas Repak nxf54684 <nxf54684@nxp.com>
2021-02-26 09:11:33 +0100

    download model update

--- thesis/download_model.sh
+++ thesis/download_model.sh
@@ -11,3 +11,5 @@ wget https://storage.googleapis.com/cloud-tpu-checkpoints/efficientdet/coco/effi
 wget https://user-images.githubusercontent.com/11736571/77320690-099af300-6d37-11ea-9d86-24f14dc2d540.png -O img.png
 
 tar xf efficientdet-${1}.tar.gz
+
+cp ../run.py .

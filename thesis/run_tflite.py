efdcc9fcce235f53f3a547b07f17bfb6cefdddc0
Tomas Repak nxf54684 <nxf54684@nxp.com>
2021-02-26 10:15:46 +0100

    Map input resolutions to EffDet versions

--- thesis/run_tflite.py
+++ thesis/run_tflite.py
@@ -7,6 +7,20 @@ import cv2
 import sys
 import os
 
+# Different versions of EfficientDet need different inputs
+# Map the versions to deisred inputs
+
+resolutions = {
+  "d0" : 512,
+  "d1" : 640,
+  "d2" : 768,
+  "d3" : 896,
+  "d4" : 1024,
+  "d5" : 1280,
+  "d6" : 1280,
+  "d7" : 1536
+}
+
 version = sys.argv[1]
 
 current_dir = os.getcwd()
@@ -22,7 +36,7 @@ output_details = interpreter.get_output_details()
 
 input_shape = input_details[0]['shape']
 input_img = cv2.imread(current_dir + "/" + sys.argv[2])
-input_data = cv2.resize(input_img, (512, 512))
+input_data = cv2.resize(input_img, (resolutions[version], resolutions[version]))
 interpreter.set_tensor(input_details[0]['index'], [input_data])
 
 interpreter.invoke()

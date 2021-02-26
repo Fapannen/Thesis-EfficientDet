719fa7fe60c40275dc31b7e5c9e67b428a459206
Tomas Repak nxf54684 <nxf54684@nxp.com>
2021-02-26 09:47:50 +0100

    run everything from thesis folder

--- thesis/run_tflite.py
+++ thesis/run_tflite.py
@@ -5,15 +5,23 @@ import numpy as np
 import tensorflow as tf
 import cv2
 import sys
+import os
 
-interpreter = tf.lite.Interpreter(model_path="efficientdet-d0.tflite")
+version = sys.argv[1]
+
+current_dir = os.getcwd()
+
+dir = os.chdir(os.getcwd() + "/" + version)
+model = "efficientdet-" + version + ".tflite"
+
+interpreter = tf.lite.Interpreter(model_path=model)
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 input_shape = input_details[0]['shape']
-input_img = cv2.imread(sys.argv[1])
+input_img = cv2.imread(current_dir + "/" + sys.argv[2])
 input_data = cv2.resize(input_img, (512, 512))
 interpreter.set_tensor(input_details[0]['index'], [input_data])
 

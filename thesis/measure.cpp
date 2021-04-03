4b441849bbd23b43bc5887f39fb619ca0b82ccd1
Tomas Repak nxf54684 <nxf54684@nxp.com>
2021-04-03 10:22:21 +0200

    Draw bounding boxes from outputs

--- thesis/measure.cpp
+++ thesis/measure.cpp
@@ -33,6 +33,58 @@ void printVector(const std::vector<float>& v)
   std::cout << std::endl;
 }
 
+std::vector<std::vector<float>> getOutputVectors(const TfLiteTensor* tensor_ptr,const int num_outputs,const int output_size)
+{
+  std::vector<std::vector<float>> outputs;
+
+  float* output = reinterpret_cast<float*>(tensor_ptr->data.raw);
+
+  for (int i = 0; i < num_outputs; ++i)
+  {
+    std::vector<float> outvec;
+
+    for (int j = 0; j < output_size; ++j)
+    {
+      outvec.push_back(output[(i * output_size) + j]);
+    }
+
+    outputs.push_back(outvec);
+  }
+
+  return outputs;
+}
+
+void drawBoundingBoxes(const std::vector<std::vector<float>>& outputs, cv::Mat& image)
+{
+  std::vector<std::vector<float>> boxesToDraw;
+
+  // First output will always be drawn
+  boxesToDraw.push_back(outputs[0]);
+
+  for(size_t i = 1; i < outputs.size() - 1; i++){
+    if(outputs[i] != outputs[i-1]){
+      boxesToDraw.push_back(outputs[i]);
+    }
+  }
+
+  for(std::vector<float>& vec : boxesToDraw){
+    int imgNum = vec[0];
+    int ymin   = vec[1];
+    int xmin   = vec[2];
+    int ymax   = vec[3];
+    int xmax   = vec[4];
+    int score  = vec[5];
+    int label  = vec[6];
+
+    cv::Point topRight(xmin, ymin);
+    cv::Point botLeft(xmax, ymax);
+
+    cv::rectangle(image, topRight, botLeft, cv::Scalar(0, 255, 0));
+  }
+
+  cv::imwrite("out.png", image);
+}
+
 // This is an example that is minimal to read a model
 // from disk and perform inference. There is no data being loaded
 // that is up to you to add as a user.
@@ -115,17 +167,10 @@ int main(int argc, char* argv[]) {
 
   // Output tensor is "float32 [1, 100, 7]"
   TfLiteTensor* outtensor = interpreter->output_tensor(0);
-  auto output = reinterpret_cast<float*>(outtensor->data.raw);
 
-  std::vector<float> outvec;
-  for (int i = 0; i < 100 * 7; ++i)
-  {
-    outvec.push_back(static_cast<float>(output[i]));
-    if(i % 7 == 6){
-      printVector(outvec);
-      outvec.erase(outvec.begin(), outvec.end());
-    }
-  }
+  auto outputs = getOutputVectors(outtensor, 100, 7);
+
+  drawBoundingBoxes(outputs, resized_img);
 
   printf("Just finished ...\n");
 
